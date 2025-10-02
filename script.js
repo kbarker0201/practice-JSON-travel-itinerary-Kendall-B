533685935c151b277718d9da0c5f0134edeef0dd
kbarker0201 <kbarker0201@gmail.com>
2025-10-01 19:16:04 -0500

    final changes

---
+++ script.js
@@ -0,0 +1,32 @@
+const itinerary = {
+    "tripName": "Trip to Korea",
+    "name": "Kendall",
+    "contact": {
+        "email": "kbarker0201@gmail.com",
+        "phone": "123-456-7891"
+    },
+    "firstDestination": {
+        "location": "Seoul",
+        "arrivalDate": "September 1",
+        "departureDate": "September 25",
+        "activities": [{
+            "title": "visit BTS cafe",
+            "time": "12:30"
+        }, {
+            "title": "visit YG headquarters",
+            "time": "2:00"
+        }]
+    },
+    "secondDestination": {
+        "location": "Busan",
+        "arrivalDate": "September 25",
+        "departureDate": "October 15",
+        "activities": [{
+            "title": "visit Park cafe",
+            "time": "3:00"
+        }, {
+            "title": "visit beach",
+            "time": "5:00"
+        }]
+    }
+}
